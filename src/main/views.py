96ab582421cca63cb6dc2534a0cc66024476db07
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2024-01-09 00:26:08 +0330

    update

--- src/main/views.py
+++ src/main/views.py
@@ -19,12 +19,6 @@ def index():
 def git_update():
     repo = Repo("~/Flask-Blog-Project")
     origin = repo.remotes.origin
-
-    # Check out to the 'main' branch (assumed that 'main' is the branch name)
     repo.heads.main.checkout()
-
-    # Pull the latest changes from the origin
     origin.pull()
-
-    # Return a success response
     return "", 200
