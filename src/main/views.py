8033b6b2745145a21ef0ddda94d96ab6f6b6edac
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2024-01-09 00:01:35 +0330

    update

--- src/main/views.py
+++ src/main/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template
-from git import Repo
 
-from . import csrf, services
+from . import services
 from .models import Post
 
 main = Blueprint("main", __name__, static_folder="static")
@@ -24,19 +23,3 @@ def index():
 #     ).checkout()
 #     origin.pull()
 #     return "", 200
-
-
-@main.route("/git-update", methods=["POST"])
-@csrf.exempt
-def git_update():
-    repo = Repo("./Flask-Blog-Project")
-    origin = repo.remotes.origin
-
-    # Check out to the 'main' branch (assumed that 'main' is the branch name)
-    repo.heads.main.checkout()
-
-    # Pull the latest changes from the origin
-    origin.pull()
-
-    # Return a success response
-    return "", 200
--- src/run.py
+++ src/run.py
@@ -1,6 +1,24 @@
-from main import create_app
+from git import Repo
+
+from main import create_app, csrf
 
 app = create_app()
+
+
+@app.route("/git-update", methods=["POST"])
+@csrf.exempt
+def git_update():
+    repo = Repo("./Flask-Blog-Project")
+    origin = repo.remotes.origin
+
+    # Check out to the 'main' branch (assumed that 'main' is the branch name)
+    repo.heads.main.checkout()
+
+    # Pull the latest changes from the origin
+    origin.pull()
+
+    # Return a success response
+    return "", 200
 
 
 if __name__ == "__main__":
